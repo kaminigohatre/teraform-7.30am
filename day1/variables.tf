c68bfe96976b5217766eee4c244f27259ae3aa92
kamini <kaminigohatre05255@gmail.com>
2025-06-20 09:14:26 +0530

    day1

---
+++ day1/variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    description = "inserting ami value from main.tf"  # optional
+    type= string
+    default = "ami-09e6f87a47903347c"
+  
+}
+
+variable "type" {
+    type = string
+    default = "t2.micro"
+  
+}
+variable "keyname" {
+    type = string
+    default = "31may"
+  
+}
