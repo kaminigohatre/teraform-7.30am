f02e3f2b3bfff87ddf0997d578f78defdfacbeba
kamini <kaminigohatre05255@gmail.com>
2025-06-21 15:24:25 +0530

    modify day3

--- day3statefile/main.tf
+++ day3statefile/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "dev" {
     ami = "ami-09e6f87a47903347c"
     instance_type = "t2.micro"
     key_name = "uskey"
-    # availability_zone = "ap-south-1b"
+    availability_zone = "us-east-1a"
     # tags = {
     #     Name = "dev"
     # }
